fe738007eb68d38698b0c24a1ba458399fabd9f6
sudalqueen <abbey17@cnu.ac.kr>
2020-12-30 20:58:16 +0900

    feature: Login 컴포넌트 layout 잡는 중

--- src/container/Login.tsx
+++ src/container/Login.tsx
@@ -1,27 +1,38 @@
-import React from 'react';
-import { Button, Input, Space, Typography } from 'antd';
+import React, {useEffect} from 'react';
+import { Button, Input, Space, Typography, Layout } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone, UserOutlined, LockOutlined  } from '@ant-design/icons';
 
 const Login: React.FC = () => {
     const { Title } = Typography;
+    const { Header, Footer, Content } = Layout;
 
     return (
-        <div>
-            <Space direction="vertical">
-                <Title>떡볶기획</Title>
-                <Input
-                    placeholder="Enter your username"
-                    prefix={<UserOutlined className="site-form-item-icon" />} />
-                <Input.Password
-                    placeholder="password"
-                    prefix={<LockOutlined />}
-                    iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
-                />
-                <Button type="primary">로그인</Button>
-                <Button type="link">비밀번호를 잊으셨나요?</Button>
-                <Button type="link">회원가입</Button>
-            </Space>
-        </div>
+        <Layout>
+            <Header>
+                <title>떡볶기획</title>
+            </Header>
+            <Content>
+                <Space direction="vertical" size={100}>
+                    <Title>Sign in</Title>
+                    <Space direction="vertical" size="small">
+                        <Input
+                            placeholder="Enter your username"/>
+                        <Input.Password
+                            placeholder="password"
+                            iconRender={visible => (visible ? <EyeTwoTone /> : <EyeInvisibleOutlined />)}
+                        />
+                        <Button type="primary">로그인</Button>
+                    </Space>
+                    <div>
+                        <Button type="link">비밀번호를 잊으셨나요?</Button>
+                        <Button type="link">회원가입</Button>
+                    </div>
+                </Space>
+            </Content>
+            <Footer>
+
+            </Footer>
+        </Layout>
     )
 };
 
