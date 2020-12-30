2d401ce8a1608465f77857383433dec1866846e8
sudalqueen <abbey17@cnu.ac.kr>
2020-12-30 15:43:16 +0900

    feature: http 통신을 위한 BaseAdapter 작성
    (cherry picked from commit 880640548b2d663d5d907f00fc9a7b07e59fc3b0)

---
+++ src/adapter/BaseAdapter.ts
@@ -0,0 +1,23 @@
+import axios, {AxiosInstance} from "axios";
+
+function BaseAdapter(defaultConfig: {}) {
+    const instance: AxiosInstance = axios.create({
+        ...defaultConfig,
+        timeout: 5000,
+        headers: {'Content-Type': 'application/json'}
+    });
+
+    instance.interceptors.request.use((response) => response.data,
+        (error) => {
+            if (error.response) {
+                throw error.response.data;
+            } else {
+                throw new Error();
+            }
+        }
+    );
+
+    return instance;
+}
+
+export default BaseAdapter;
