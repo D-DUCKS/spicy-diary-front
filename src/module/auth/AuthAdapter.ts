4f3b3bfeea16404a411c3cf6497b57428d04fecf
sudalqueen <abbey17@cnu.ac.kr>
2020-12-30 15:43:39 +0900

    feature: 로그인을 위한 AuthAdapter 작성 중

---
+++ src/module/auth/AuthAdapter.ts
@@ -0,0 +1,17 @@
+import {AxiosInstance} from "axios";
+import BaseAdapter from "adapter/BaseAdapter";
+
+export default class AuthAdapter {
+    private adapter: AxiosInstance;
+
+    constructor() {
+        this.adapter = BaseAdapter({baseURL: "http://localhost:8080/auth"});
+    }
+
+    login = (id: string, pwd: string) => {
+        return this.adapter.post(`/auth`, {
+            id,
+            pwd
+        });
+    }
+}
