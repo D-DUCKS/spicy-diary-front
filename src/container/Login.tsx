2695a52782a680321e4fa73b55e8f30b38dd20e9
jayjay.egg <jayjay.egg@kakaoenterprise.com>
2021-01-21 20:43:41 +0900

    feat: Login.tsx 추가 및 css 연결

--- src/container/Login.tsx
+++ src/container/Login.tsx
@@ -1,20 +1,21 @@
-import React, {useEffect} from 'react';
+import React from 'react';
 import { Button, Input, Space, Typography, Layout } from 'antd';
 import { EyeInvisibleOutlined, EyeTwoTone, UserOutlined, LockOutlined  } from '@ant-design/icons';
+import "../css/Login.css";
 
 const Login: React.FC = () => {
     const { Title } = Typography;
     const { Header, Footer, Content } = Layout;
 
     return (
-        <Layout>
-            <Header>
-                <title>떡볶기획</title>
+        <Layout className="login-layout">
+            <Header className="login-header">
+                <Title>DDucks</Title>
             </Header>
-            <Content>
-                <Space direction="vertical" size={100}>
+            <Content className="login-content">
+                <Space className="login-content_space" direction="vertical" size={100}>
                     <Title>Sign in</Title>
-                    <Space direction="vertical" size="small">
+                    <Space className="login-content-input-wrapper" direction="vertical" size="small">
                         <Input
                             placeholder="Enter your username"/>
                         <Input.Password
